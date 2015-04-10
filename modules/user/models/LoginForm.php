b23c89862d7ccb155525e63783061e1e57de5444
Victor <devmosedu@yandex.ru>
2015-04-10 16:18:48 +0300

    Change login form

--- modules/user/models/LoginForm.php
+++ modules/user/models/LoginForm.php
@@ -56,10 +56,15 @@ class LoginForm extends Model
         if (!$this->hasErrors()) {
             $user = $this->getUser();
 //            Yii::info( $user ? ("login user = " . print_r($user->attributes, true)) : 'login no user');
-            $user->setPassword('1111');
-
-            if (!$user || !$user->validatePassword($this->password)) {
+            if( !$user ) {
                 $this->addError($attribute, 'Incorrect username or password.');
+            }
+            else {
+                $user->setPassword('1111');
+
+                if (!$user || !$user->validatePassword($this->password)) {
+                    $this->addError($attribute, 'Incorrect username or password.');
+                }
             }
         }
     }
